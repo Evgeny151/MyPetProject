6a5dd0015e673cf5ad5699c02fd5f2c6b7c990ec
Evgeny Krechko <zgreka@yandex.ru>
2021-11-29 23:38:05 +0300

    validation req body

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,18 @@
+const { validationResult } = require('express-validator')
+
+const ApiError = require('../exceptions/api-error')
 const userService = require('../service/user-service')
 
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(
+                    ApiError.BadRequest('Ошибка при валидации', errors.array())
+                )
+            }
+
             const { email, password } = req.body
             const userData = await userService.registration(email, password)
 
